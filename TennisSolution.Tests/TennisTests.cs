607e2223a1147dd7e3394ddb2789c9485ae23dc5
Brandon <brandonwijnands@live.nl>
2025-11-25 13:30:49 +0100

    chore: added full game test

--- TennisSolution.Tests/TennisTests.cs
+++ TennisSolution.Tests/TennisTests.cs
@@ -67,4 +67,29 @@ public class TennisTests
         string value = tennisGame.playerScores(2);
         Assert.Equal("Player two scored! Player two wins", value);
     }
+    [Fact]
+    public void FullGameTest()
+    {
+        TennisGame tennisGame = new TennisGame(0, 0);
+        string value = tennisGame.playerScores(2);
+        Assert.Equal("Player two scored! Love - 15", value);
+        value = tennisGame.playerScores(2);
+        Assert.Equal("Player two scored! Love - 30", value);
+        value = tennisGame.playerScores(1);
+        Assert.Equal("Player one scored! 15 - 30", value);
+        value = tennisGame.playerScores(1);
+        Assert.Equal("Player one scored! 30 - 30", value);
+        value = tennisGame.playerScores(2);
+        Assert.Equal("Player two scored! 30 - 40", value);
+        value = tennisGame.playerScores(1);
+        Assert.Equal("Player one scored! Deuce", value);
+        value = tennisGame.playerScores(2);
+        Assert.Equal("Player two scored! Player two has advantage", value);
+        value = tennisGame.playerScores(1);
+        Assert.Equal("Player one scored! Deuce", value);
+        value = tennisGame.playerScores(1);
+        Assert.Equal("Player one scored! Player one has advantage", value);
+        value = tennisGame.playerScores(1);
+        Assert.Equal("Player one scored! Player one wins", value);
+    }
 }
