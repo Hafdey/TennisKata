5be13a77ddc2f9aeeb725d93a8da1771bafd66b1
Brandon <brandonwijnands@live.nl>
2025-11-25 13:15:26 +0100

    feat: added winning functionality

--- TennisSolution/TennisGame.cs
+++ TennisSolution/TennisGame.cs
@@ -14,6 +14,33 @@ public class TennisGame
 
     public string playerScores(int player)
     {
+        if (player == 1)
+        {
+            playerOneScore++;
+            return $"Player one scored! {getScore()}";
+        }
+        playerTwoScore++;
+        return $"Player two scored! {getScore()}";
+    }
+
+    public string getScore()
+    {
+        int difference = playerOneScore - playerTwoScore;
+        //Check for a winner
+        if (playerOneScore >= 4 && difference >= 2)
+        {
+            return "Player one wins";
+        }
+        else if (playerTwoScore >= 4 && difference <= -2)
+        {
+            return "Player two wins";
+        }
+
+        //Check for a deuce
+
+        //Check for advantage
+
+        //No checks return, return score
         return "";
     }
 
