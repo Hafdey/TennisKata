8988b4b44feb765b7c222b0337e6b41dcc35b89d
Brandon <brandonwijnands@live.nl>
2025-11-25 13:24:25 +0100

    feat: added scoring, deuce and advantage

--- TennisSolution/TennisGame.cs
+++ TennisSolution/TennisGame.cs
@@ -6,6 +6,7 @@ public class TennisGame
 {
     private int playerOneScore { get; set; }
     private int playerTwoScore { get; set; }
+    private List<string> values = new List<string> { "Love", "15", "30", "40" };
     public TennisGame(int player1, int player2)
     {
         playerOneScore = player1;
@@ -26,6 +27,7 @@ public class TennisGame
     public string getScore()
     {
         int difference = playerOneScore - playerTwoScore;
+
         //Check for a winner
         if (playerOneScore >= 4 && difference >= 2)
         {
@@ -37,11 +39,24 @@ public class TennisGame
         }
 
         //Check for a deuce
+        else if (difference == 0 && playerOneScore >= 3)
+        {
+            return "Deuce";
+        }
 
         //Check for advantage
-
+        //Player one advantage
+        else if (playerOneScore >= 3 && playerTwoScore >= 3 && difference == 1)
+        {
+            return "Player one has advantage";
+        }
+        //Player two advantage
+        else if (playerTwoScore >= 3 && playerOneScore >= 3 && difference == -1)
+        {
+            return "Player two has advantage";
+        }
         //No checks return, return score
-        return "";
+        return $"{values[playerOneScore]} - {values[playerTwoScore]}";
     }
 
 }
